521b3da6505716ae72f421c83737446e1da97756
minnnisu <mine52100@naver.com>
2023-11-08 12:18:24 +0900

    ADD: mysql 연결

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+const createError = require("http-errors");
 const express = require("express");
+const dotenv = require("dotenv");
+const mysql = require("mysql");
+const db = require("./api/db/db_connect");
 const app = express();
+
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+dotenv.config();
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.send(err.message);
 });
 
 app.listen(port, () => {
