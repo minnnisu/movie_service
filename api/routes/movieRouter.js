2e37288a201d06b11555dcf09b258019040112ad
minnnisu <mine52100@naver.com>
2023-11-12 14:39:48 +0900

    FIX: 개봉일자 포맷방식 변경

--- api/routes/movieRouter.js
+++ api/routes/movieRouter.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const movieController = require("../../controller/movieController");
 
-router.get("/:title", movieController.getMovieInfoPage);
+router.get("/movieDetail/:title", movieController.getMovieInfoPage);
 
 module.exports = router;
--- controller/movieController.js
+++ controller/movieController.js
@@ -3,14 +3,12 @@ const createError = require("http-errors");
 
 exports.getMovieInfoPage = async function (req, res, next) {
   const connection = await pool.getConnection();
-  console.log(req.params.title);
 
   try {
     const [rows] = await connection.query(
-      `SELECT poster_image, title, summary, running_time, age_limit, released_date FROM movies WHERE title = ?`,
+      `SELECT poster_image, title, summary, running_time, age_limit, DATE_FORMAT(released_date, '%Y-%m-%d') AS released_date FROM movies WHERE title = ?`,
       [req.params.title]
     );
-    console.log({ movies: rows });
     res.render("movie_info.ejs", { movies: rows });
   } catch (err) {
     console.log(err);
