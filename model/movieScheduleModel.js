b5e9a16515fb4d78729d6a6b3fe13775ff56a58f
minnnisu <mine52100@naver.com>
2023-11-14 13:59:53 +0900

    FIX: 코드 수정

--- model/movieScheduleModel.js
+++ model/movieScheduleModel.js
@@ -4,8 +4,8 @@ exports.getMovieSchedule = async function (date) {
   const connection = await pool.getConnection();
   try {
     const [rows] = await connection.query(
-      `SELECT title, room_id, DATE_FORMAT(start_time, '%Y-%m-%d %H:%i') AS start_time, 
-            DATE_FORMAT(end_time, '%Y-%m-%d %H:%i') AS end_time, 
+      `SELECT title, room_id, DATE_FORMAT(start_time, '%H:%i') AS start_time, 
+            DATE_FORMAT(end_time, '%H:%i') AS end_time, 
             ordered_seat_count FROM movieSchedule WHERE start_time BETWEEN ? AND ? ORDER BY title, room_id, start_time;`,
       [`${date} 00:00`, `${date} 23:59`]
     );
