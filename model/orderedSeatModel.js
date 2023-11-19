6373265a0fb28394ee5ab85af88b2977dd830d8a
minnnisu <mine52100@naver.com>
2023-11-20 02:00:20 +0900

    FIX: 하드코딩 버그 수정

--- model/orderedSeatModel.js
+++ model/orderedSeatModel.js
@@ -4,7 +4,7 @@ exports.getOrderedSeatsByMovieTime = async function (movieTimeId) {
   const connection = await pool.getConnection();
   const [rows] = await connection.query(
     `SELECT seat_row, seat_col 
-    FROM (SELECT * from orders where movie_time_id = 17) as orders, orderedSeats 
+    FROM (SELECT * from orders where movie_time_id = ?) as orders, orderedSeats 
     WHERE orders.order_id = orderedSeats.order_id AND canceled_at is NULL;`,
     [movieTimeId]
   );
