cea9fb599984ca4afe2d95f3e6698ea22c6730b1
minnnisu <mine52100@naver.com>
2023-11-20 01:24:34 +0900

    ADD: 마이페이지 구현

--- api/routes/pages/userRouter.js
+++ api/routes/pages/userRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../../../controller/userController");
 const authMiddleware = require("../../middleware/authMiddleware");
 
-router.get("/", authMiddleware.isLoginStatus, userController.getUser);
+router.get("/profile", authMiddleware.isLoginStatus, userController.getUser);
 // router.get(
 //   "/order",
 //   authMiddleware.isLoginStatus,
--- controller/userController.js
+++ controller/userController.js
@@ -3,7 +3,7 @@ const userService = require("../service/userService");
 exports.getUser = async function (req, res, next) {
   try {
     const user = await userService.getUser(req.user);
-    return res.status(200).json(user);
+    return res.render("my_page", { user });
   } catch (error) {
     return next(error);
   }
